771088ed59af1196962e5b71ff19fba830e84ea3
Yulong Zhuang <zhuangyu@cfelm-pcx31503.desy.de>
2020-12-09 09:05:27 +0100

    test edge smoothing analysis

--- Sphere_edging_analysis.py
+++ Sphere_edging_analysis.py
@@ -7,70 +7,118 @@ import ctypes
 from scipy.signal import savgol_filter
 from scipy.interpolate import interp1d
 import multiprocessing as mp
+import h5py
+from scipy.signal import argrelextrema
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+%matplotlib
+
+
+##mask_emc = np.array([ not i for i in mask_emc0]).reshape(319, 331)                        #mask file
 
 #poisson
 det = detector.Detector('det_2160_lowq8.h5', mask_flag=True)
+det_mask = det.unassembled_mask
+rad_map = np.ceil(np.sqrt(det.cx**2+det.cy**2))
+rad_masked = (rad_map[det_mask == 1])
+rad_masked = rad_masked.astype(int)
 #with open('photons_corr.txt', 'r') as f:
 #     emc_flist = [i.strip() for i in f.readlines()]
 
-emc_flist = '../../emc/corr/cub42_lowq.emc'
+
+emc_flist = '../../emc/scorr/cub42_lowq.emc'
 
 emc = reademc.EMCReader(emc_flist,det)
 n_frames = emc.num_frames
 
-clist = np.array([f['num_data'] for f in emc.flist])
-running_num = np.zeros(np.max(clist))
-running_num[0:clist[0]] = np.int( emc_flist[0][75:78] )
-for i in range(len(clist)-1):
-    running_num[clist[i]:clist[i+1]] = np.int( emc_flist[i+1][75:78] )
+h5 = h5py.File('/gpfs/exfel/u/scratch/SPB/201802/p002160/yulong/cub42_0001/data/cub_sph/Whole_sample7.h5', 'r')
+pm0 = h5['PCA_mean_0'][:]
+pm1 = h5['PCA_mean_1'][:]
+pm2 = h5['PCA_mean_2'][:]
+h5.close()
+nsp = np.where((pm0 < 0) & (pm1 > 1.35))[0]
 
 
 
-mask_emc0 = emc.get_frame(1).mask.ravel()
-mask_emc0 = np.array([ not i for i in mask_emc0]).reshape(512, 256)
-mask_center = np.load('../aux/mask_center_cmc_0.npy')                           #mask file
-
-intrad = np.load('../aux/intrad_004.npy')
-intrad = intrad[256:768,256:512]
-flotrad = intrad[mask_center]
-s_s = np.load('../aux/int2std_model_r30_mask0_weighted.npz')
-std_models = s_s['std_model']
-int_models = s_s['intens_model']
-std_smooth = savgol_filter(std_models, 51, 3)
-fis = interp1d(np.log10(int_models), std_smooth,'cubic')
-
-
-radcount = np.zeros(intrad.max() + 1)
-np.add.at(radcount, intrad, mask_center)
+frame_p = emc.get_frame(0, raw=True)*0+1
+radcount = np.zeros(rad_masked.max() + 1)
+np.add.at(radcount, rad_masked, frame_p[det_mask==1])
 radmask = (radcount > 10)
 radcount[radcount == 0] = 1
+radii = np.arange(220)
 
-det_dist = 100 # mm
-qvals = 2. * np.sin(0.5 * np.arctan(np.arange(int(intrad.max())+1) * 0.075 / det_dist)) / 4.5
-diameters = np.linspace(10, 50, 1200)
+
+
+qvals = 2. * np.sin(0.5 * np.arctan(np.arange(220)*0.2/705)) * 6/12.3984 * 10
+diameters = np.arange(30,80,0.1)
 svals = np.outer(diameters, qvals) * np.pi
 svals[svals==0] = 1e-6
 spheremodels = (np.sin(svals) - svals * np.cos(svals))**2 / svals**6 * diameters[:,np.newaxis]**6
 
+n_frames = len(nsp)
+cc = np.zeros(n_frames)
+dia = np.zeros(n_frames)
+Rq = np.zeros((n_frames, 100))
+Profile_obs = np.zeros((n_frames, 100))
+Profile_ideal = np.zeros((n_frames, 100))
+evenlop_obs = np.zeros((n_frames, 100))
+evenlop_ideal = np.zeros((n_frames, 100))
+index = np.zeros(n_frames)
+index0 = np.zeros(n_frames)
+for i in range(100):
+    f_i = emc.get_frame(nsp[i], raw=True)
+    data_i = f_i[det_mask==1]
+    radavg = np.zeros_like(radcount)
+    tdata = data_i
+    np.add.at(radavg, rad_masked, tdata)
+    radavg /= radcount
 
+    corrs = np.corrcoef(radavg[20:120], spheremodels[:,20:120])[0, 1:]
+    Profile_obs[i] = radavg[20:120]/np.max(radavg[20:120])
+    Profile_ideal[i] = spheremodels[corrs.argmax(),20:120]/np.max(spheremodels[corrs.argmax(),20:120])
+    pf_obs_max = argrelextrema(Profile_obs[i], np.greater)[0]
+    pf_ideal_max = argrelextrema(Profile_ideal[i], np.greater)[0]
+    evenlop_obs[i] =  np.interp(radii[20:120],pf_obs_max,Profile_obs[i][pf_obs_max])
+    evenlop_ideal[i] =  np.interp(radii[20:120],pf_ideal_max,Profile_ideal[i][pf_ideal_max])
+    cc[i] = corrs.max()
+    dia[i] = diameters[corrs.argmax()]
+    Rq[i] = Profile_obs[i]/ Profile_ideal[i]
+    index[i] = np.sum(Rq[i]*radii[20:120]**2)/np.sum(Rq[i])
+    index0[i] = np.sum(Profile_obs[i])/np.sum(Profile_ideal[i])
+
+plt.plot(evenlop_obs[i], color='blue')
+plt.plot(Profile_obs[i], color='blue')
+
+plt.plot(evenlop_ideal[i], color='red')
+plt.plot(Profile_ideal[i], color='red')
+plt.yscale('log')
+
+plt.plot(Rq[i])
+
+
+
+print(index[i])
+
+
+#np.add.at(radcount, rad_masked, view1[det_mask==1])
 ###############################################
 intens_tot = mp.Array(ctypes.c_double, n_frames)
 cc = mp.Array(ctypes.c_double, n_frames)
 error = mp.Array(ctypes.c_double, n_frames)
 dia = mp.Array(ctypes.c_double, n_frames)
-
+Rq = np.Array(ctypes.c_double, n_frames, )
 def mp_worker(rank, indices, dia, intens_tot, cc, error):
     irange = indices[rank::nproc]
 
     for i in irange:
-        f_i = emc.get_frame(i)
+        f_i = emc.get_frame(i, raw=True)
         data_i = f_i.data[mask_center]
         radavg = np.zeros_like(radcount)
         tdata = data_i
         np.add.at(radavg, flotrad, tdata)
         radavg /= radcount
 
-        corrs = np.corrcoef(((radcount*radavg)[radmask]), (radcount*spheremodels)[:,radmask])[0, 1:]
+        corrs = np.corrcoef(((radcount)[20:120]), (spheremodels)[:,20*120])[0, 1:]
         cc[i] = corrs.max()
         dia[i] = diameters[corrs.argmax()]
         intens_tot[i] = np.sum(tdata)
